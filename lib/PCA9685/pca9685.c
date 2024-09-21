86256fc55d64068f29b42fda85b7d36752b14495
Szymon Hudziak <szymonh.dg@gmail.com>
2024-09-21 18:23:37 +0200

    Add homing and sample move

--- lib/PCA9685/pca9685.c
+++ lib/PCA9685/pca9685.c
@@ -1,10 +1,10 @@
 #include "pca9685.h"
 
-#define I2C_MASTER_SCL_IO 22      //GPIO number for I2C master clock
-#define I2C_MASTER_SDA_IO 21      //GPIO number for I2C master data
-#define I2C_MASTER_FREQ_HZ 100000 //I2C master clock frequency
-#define I2C_MASTER_NUM I2C_NUM_0  //I2C port number for master dev
-#define I2C_MASTER_TIMEOUT_MS 1000 //I2C timeout in ms
+#define I2C_MASTER_SCL_IO 22       // GPIO number for I2C master clock
+#define I2C_MASTER_SDA_IO 21       // GPIO number for I2C master data
+#define I2C_MASTER_FREQ_HZ 100000  // I2C master clock frequency
+#define I2C_MASTER_NUM I2C_NUM_0   // I2C port number for master dev
+#define I2C_MASTER_TIMEOUT_MS 1000 // I2C timeout in ms
 
 #define PCA9685_ADDR 0x40 // Default PCA9685 address
 #define SERVO_MIN_US 500  // Minimum pulse width in µs
--- src/main.c
+++ src/main.c
@@ -1,1 +1,58 @@
-void app_main() {}
+#include "pca9685.h"
+
+uint16_t home_position[] = {90, 135, 30, 90, 0};
+uint16_t current_position[5];
+uint16_t target_position[] = {30, 90, 0, 135, 180};
+
+void move_home()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        pca9685_set_servo_angle(i, home_position[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        current_position[i] = home_position[i];
+    }
+}
+void sample_move()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        while (current_position[i] != target_position[i])
+        {
+            if (target_position[i] - current_position[i] > 0)
+            {
+                pca9685_set_servo_angle(i, current_position[i] + 1);
+                current_position[i] = current_position[i] + 1;
+                vTaskDelay(pdMS_TO_TICKS(20));
+            }
+            else if (target_position[i] - current_position[i] < 0)
+            {
+                pca9685_set_servo_angle(i, current_position[i] - 1);
+                current_position[i] = current_position[i] - 1;
+                vTaskDelay(pdMS_TO_TICKS(20));
+            }
+        }
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(2000));
+
+    for (int i = 0; i < 5; i++)
+    {
+        target_position[i] = home_position[i];
+    }
+}
+
+void app_main(void)
+{
+    i2c_master_init(); // Initialize I2C
+    pca9685_init();    // Initialize PCA9685
+
+    move_home();
+
+    while (true)
+    {
+        sample_move();
+    }
+}
