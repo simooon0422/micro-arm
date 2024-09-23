a8b21bf717db6943f2a25c0dfdba9d6698940242
Szymon Hudziak <szymonh.dg@gmail.com>
2024-09-23 13:27:08 +0200

    Add simultaneous servo movement

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 static const char *TAG = "ServoControl";
 uint8_t links_number = 4;
 uint8_t gripper_channel = 4;
+bool position_flag = 0;
 
 uint16_t home_position[] = {90, 135, 30, 90};
 uint16_t current_position[4];
@@ -45,45 +46,51 @@ void move_home()
 void servo_control_task(void *pvParameter)
 {
     move_home();
+    vTaskDelay(pdMS_TO_TICKS(2000));
+
     while (1)
     {
-        ESP_LOGI(TAG, "Target position:");
-        for (int i = 0; i < links_number; i++)
+        if (position_flag == 0)
         {
-            ESP_LOGI(TAG, "Servo %d: %d", i, target_position[i]);
-        }
-
-        for (int i = 0; i < links_number; i++)
-        {
-            while (current_position[i] != target_position[i])
+            ESP_LOGI(TAG, "Target position:");
+            for (int i = 0; i < links_number; i++)
             {
-                if (target_position[i] - current_position[i] > 0)
+                ESP_LOGI(TAG, "Servo %d: %d", i, target_position[i]);
+            }
+
+            int move_step = 0;
+
+            for (int i = 0; i < links_number; i++)
+            {
+                if (current_position[i] != target_position[i])
                 {
-                    pca9685_set_servo_angle(i, current_position[i] + 1);
-                    current_position[i] = current_position[i] + 1;
+                    move_step = (target_position[i] - current_position[i]) / abs(target_position[i] - current_position[i]);
+                    pca9685_set_servo_angle(i, current_position[i] + move_step);
+                    current_position[i] = current_position[i] + move_step;
                 }
-                else if (target_position[i] - current_position[i] < 0)
+            }
+            vTaskDelay(pdMS_TO_TICKS(20));
+
+            for (int i = 0; i < links_number; i++)
+            {
+                if (current_position[i] != target_position[i])
                 {
-                    pca9685_set_servo_angle(i, current_position[i] - 1);
-                    current_position[i] = current_position[i] - 1;
-                }
-                vTaskDelay(pdMS_TO_TICKS(20));
+                    position_flag = 0;
+                    break;
+                }     
+                position_flag = 1;         
+                ESP_LOGI(TAG, "Target reached");
             }
         }
 
-        ESP_LOGI(TAG, "Target reached");
-
-        gripper_close();
-
-        vTaskDelay(pdMS_TO_TICKS(2000));
-
-        gripper_open();
-
-        vTaskDelay(pdMS_TO_TICKS(2000));
-
-        for (int i = 0; i < links_number; i++)
+        if (position_flag == 1)
         {
-            target_position[i] = home_position[i];
+            vTaskDelay(pdMS_TO_TICKS(2000));
+            for (int i = 0; i < links_number; i++)
+            {
+                target_position[i] = home_position[i];
+            }
+            position_flag = 0;
         }
     }
 }
