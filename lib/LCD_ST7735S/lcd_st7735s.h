9f619ccd8a97f950a1a812cfe0d9d8b00a47bc11
Szymon Hudziak <szymonh.dg@gmail.com>
2024-11-01 16:06:51 +0100

    Prepare tests for lcd_st7735s library

--- lib/LCD_ST7735S/lcd_st7735s.h
+++ lib/LCD_ST7735S/lcd_st7735s.h
@@ -4,20 +4,29 @@
 
 /**
  * @brief Initialize SPI bus.
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
  */
-void init_spi();
+esp_err_t init_spi();
 
 /**
  * @brief Send SPI command.
  * @param cmd Command to send.
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
  */
-void send_command(uint8_t cmd);
+esp_err_t lcd_send_command(uint8_t cmd);
 
 /**
  * @brief Send data through SPI.
  * @param data Data to send.
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
  */
-void send_data(uint8_t data);
+esp_err_t lcd_send_data(uint8_t data);
 
 /**
  * @brief Initialize display with st7735 driver.
@@ -30,4 +39,4 @@ void st7735_init();
  * @param y Y coordinate of pixel.
  * @param color Color of pixel
  */
-void draw_pixel(uint16_t x, uint16_t y, uint16_t color);
+void lcd_draw_pixel(uint16_t x, uint16_t y, uint16_t color);
---
+++ test/test_lcd_st7735s/test_lcd_st7735s.c
@@ -0,0 +1,49 @@
+#include <unity.h>
+#include "lcd_st7735s.h"
+
+void test_init_OK(void)
+{
+    esp_err_t ret = init_spi();
+    TEST_ASSERT_EQUAL(ESP_OK, ret);
+}
+
+void test_send_command_NOK(void)
+{
+    esp_err_t ret = lcd_send_command("test");
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, ret);
+}
+
+void test_send_data_NOK(void)
+{
+    esp_err_t ret = lcd_send_data("test");
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, ret);
+}
+
+void test_send_command_OK(void)
+{
+    esp_err_t ret = lcd_send_command(0x3A);
+    TEST_ASSERT_EQUAL(ESP_OK, ret);
+}
+
+void test_send_data_OK(void)
+{
+    esp_err_t ret = lcd_send_command(0x05);
+    TEST_ASSERT_EQUAL(ESP_OK, ret);
+}
+
+int runUnityTests(void)
+{
+    vTaskDelay(pdMS_TO_TICKS(2500));
+    UNITY_BEGIN();
+    RUN_TEST(test_init_OK);
+    RUN_TEST(test_send_command_NOK);
+    RUN_TEST(test_send_data_NOK);
+    RUN_TEST(test_send_command_OK);
+    RUN_TEST(test_send_data_OK);
+    return UNITY_END();
+}
+
+void app_main()
+{
+    runUnityTests();
+}
