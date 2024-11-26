6382533568e386058eeb5eb56bf72159d4e6f46a
Szymon Hudziak <szymonh.dg@gmail.com>
2024-11-26 18:47:31 +0100

    Modify HAGL library location in project and fix multiple definitions error

---
+++ lib/HAGL/include/font6x9.h
@@ -0,0 +1,8 @@
+/*
+
+FONTX version of the Public Domain X11 misc-fixed typeface.
+https://www.cl.cam.ac.uk/~mgk25/ucs-fonts.html
+
+*/
+extern const unsigned char font6x9[];
+extern unsigned int font6x9_size;
